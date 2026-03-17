0967fb73fbadeb55068276fb6ee0e83999b349ad
Renildo15 <renildorabi@ufrn.edu.br>
2026-03-16 21:43:43 -0300

    feat:criando tipagem

---
+++ types/post.ts
@@ -0,0 +1,21 @@
+export type PostType = {
+    id: number;
+    title: string;
+    content: string;
+    image?: string;
+    authorId: string;
+    createdAt: string;
+    authorName: string;
+    likeCount: number;
+}
+
+export type PostsType = {
+    posts: PostType[];
+    total: number;
+    page:number;
+    limit: number
+}
+
+export type PostUpdateType = Partial<
+  Omit<PostType, "id" | "authorId" | "createdAt" | "authorName" | "likeCount">
+>
---
+++ types/user.ts
@@ -0,0 +1,11 @@
+export type UserWithTokenType = {
+    token: string;
+    user: User
+
+}
+
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+}
